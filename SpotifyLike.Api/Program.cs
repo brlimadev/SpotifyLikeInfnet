18e7c582801e902687eee878574fc40a478dd1e5
brlim <brlim@DESKTOP-IH8RF1P>
2024-02-25 16:52:06 -0300

    Feat: configurando o dbContext e o banco de dados

--- SpotifyLike.Api/Program.cs
+++ SpotifyLike.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SpotifyLike.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SpotifyLikeContext>(c =>
+{
+    c.UseSqlServer(builder.Configuration.GetConnectionString("SpotifyConnection"));
+
+});
 
 var app = builder.Build();
 
