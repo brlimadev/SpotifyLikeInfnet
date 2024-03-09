9d378be67caffa8042b5a86507af13d2c132d59f
Bruno <Bruno@DESKTOP-NTBD3M9>
2024-03-09 08:55:41 -0800

    Comentário de include com EF

--- SpotifyLike.Repository/Repository/UsuarioRepository.cs
+++ SpotifyLike.Repository/Repository/UsuarioRepository.cs
@@ -19,5 +19,14 @@ namespace SpotifyLike.Repository.Repository
             Context = context;
         }
 
+        //public Usuario GetById(Guid id)
+        //{
+        //    return this.Context.Usuarios
+        //               .Include(x => x.Assinaturas) //Caso não esteja usando lazy loading
+        //               .Include(x => x.Playlists)
+        //               .Include(x => x.Notificacoes)
+        //               //.AsSplitQuery() //Quebra a consulta por cada tipo
+        //               .FirstOrDefault(x => x.Id == id);
+        //}
     }
 }
