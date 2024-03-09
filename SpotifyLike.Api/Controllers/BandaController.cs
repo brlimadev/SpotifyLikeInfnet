7eeb520390a54a23be7b1b9a042115b06a50ccba
Bruno <Bruno@DESKTOP-NTBD3M9>
2024-03-09 09:03:51 -0800

    Criando a api de banda

--- SpotifyLike.Api/Controllers/BandaController.cs
+++ SpotifyLike.Api/Controllers/BandaController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Spotify.Application.Streaming;
+using Spotify.Application.Streaming.Dto;
 using SpotifyLike.Domain.Streaming.Aggregates;
 using SpotifyLike.Repository;
 
@@ -9,46 +11,69 @@ namespace SpotifyLike.Api.Controllers
     [ApiController]
     public class BandaController : ControllerBase
     {
-        private SpotifyLikeContext Context { get; set; }
+        private BandaService _bandaService;
 
-        public BandaController(SpotifyLikeContext context)
+        public BandaController(BandaService bandaService)
         {
-            Context = context;
+            _bandaService = bandaService;
         }
 
         [HttpGet]
         public IActionResult GetBandas()
         {
-            var result = this.Context.Bandas.ToList();
-                     
-            return Ok(result);       
-        
+            var result = this._bandaService.Obter();
+
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetBandas(Guid id)
         {
-            var result = this.Context.Bandas
-                .FirstOrDefault(x => x.Id == id);
+            var result = this._bandaService.Obter(id);
 
-            if (result == null) 
+            if (result == null)
             {
                 return NotFound();
             }
 
             return Ok(result);
-
         }
 
         [HttpPost]
-        public IActionResult SaveBandas([FromBody]Banda banda)
+        public IActionResult Criar([FromBody] BandaDto dto)
         {
-            this.Context.Bandas.Add(banda);
-            this.Context.SaveChanges();
+            if (ModelState is { IsValid: false })
+                return BadRequest();
 
+            var result = this._bandaService.Criar(dto);
 
-            return Created($"/banda/{banda.Id}", banda);
+            return Created($"/banda/{result.Id}", result);
+        }
+
+        [HttpPost("{id}/albums")]
+        public IActionResult AssociarAlbum(AlbumDto dto)
+        {
+            if (ModelState is { IsValid: false })
+                return BadRequest();
+
+            var result = this._bandaService.AssociarAlbum(dto);
+
+            return Created($"/banda/{result.BandaId}/albums/{result.Id}", result);
 
         }
+
+
+        [HttpGet("{idBanda}/albums/{id}")]
+        public IActionResult ObterAlbum(Guid idBanda, Guid id)
+        {
+            var result = this._bandaService.ObterAlbum(idBanda, id);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+
+        }
+
     }
 }
