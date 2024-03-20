249fae819b802d935c708c36af0a58085a349576
Bruno <Bruno@DESKTOP-NTBD3M9>
2024-03-20 13:37:25 -0700

    front e swagger

---
+++ SpotifyLike.Repository/Migrations/20240320182040_NomeDaBaseInicial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotifyLike.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class NomeDaBaseInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
